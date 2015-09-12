a885a6318c751a2c39e0704931980079bab029c5
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2015-09-12 10:51:15 -0600

    Moving to cljsjs/bitauth

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject bitauth "0.0.2"
+(defproject bitauth "0.0.3"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
-  :description "A Clojure/ClojureScript port of BitPay's BitAuth authentical protocol"
+  :description "A Clojure(Script) port of BitPay's BitAuth authentical protocol"
   :url "https://github.com/xcthulhu/clj-bitauth"
   :min-lein-version "2.3.4"
   :jar-exclusions [#"\.DS_Store"]
@@ -11,31 +11,25 @@
                  [org.clojure/clojurescript "1.7.48"]
                  [com.madgag.spongycastle/core "1.52.0.0"]
                  [ring/ring-core "1.4.0"]
+                 [cljsjs/bitauth "0.2.1"]
                  [base58 "0.1.0"]]
-  :node-dependencies [[bitauth "0.2.1"]
-                      [uglify-js "2.4.24"]
-                      [browserify "11.0.1"]]
   :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-npm "0.5.0"]
-            [lein-shell "0.4.1"]]
+            [com.cemerick/clojurescript.test "0.3.3"]]
   :hooks [leiningen.cljsbuild]
-  :prep-tasks [["shell" "./scripts/install_bitpay_bitauth.sh"] "javac" "compile"]
-  :clean-targets ["target/" "dist/" "lib/" "node_modules/"]
-  :cljsbuild
-  {:builds {:bitauth
-            {:source-paths ["src/cljs" "dist"]
-             :compiler
-             {:output-to "target/js/bitauth.js"
-              :source-map "target/js/bitauth.js.map"
-              :optimizations :advanced
-              :pretty-print false
-              :foreign-libs
-              [{:file "bitpay/bitauth.js"
-                :file-min "bitpay/bitauth.min.js"
-                :provides ["bitpay.bitauth"]}]}}}}
+  :cljsbuild {:builds [{:id "test"
+                        :source-paths ["src/cljs" "test"]
+                        :notify-command ["phantomjs" :cljs.test/runner "target/compiled/js/test.js"]
+                        :compiler {:optimizations :whitespace
+                                   :pretty-print true
+                                   :output-to "target/compiled/js/test.js"
+                                   :warnings {:single-segment-namespace false}}}]}
   :scm {:name "git"
         :url "https://github.com/xcthulhu/clj-bitauth.git"}
   :pom-addition [:developers [:developer
                               [:id "xcthulhu"]
                               [:name "Matthew Wampler-Doty"]]]
+  :profiles {:dev {:test-paths ["test"]
+                   :dependencies [[compojure "1.4.0"]
+                                  [http-kit "2.1.19"]
+                                  [ring/ring-defaults "0.1.5"]]}}
   :target-path "target")
--- src/cljs/bitauth/core.cljs
+++ src/cljs/bitauth/core.cljs
@@ -1,36 +1,36 @@
 (ns bitauth.core
-  (:require [bitpay.bitauth]))
+  (:require [cljsjs.bitauth]))
 
 (defn get-public-key-from-private-key
   "Generate a public key from a private key"
   ^String [^String priv-key]
-  (.getPublicKeyFromPrivateKey js/BitAuth priv-key))
+  (.getPublicKeyFromPrivateKey js/bitauth priv-key))
 
 (defn get-sin-from-public-key
   "Generate a SIN from a compressed public key"
   ^String [^String pub-key]
-  (.getSinFromPublicKey js/BitAuth pub-key))
+  (.getSinFromPublicKey js/bitauth pub-key))
 
 (defn generate-sin
   "Generate a new private key, new public key, SIN and timestamp"
   []
-  (-> js/BitAuth .generateSin (js->clj :keywordize-keys true)))
+  (-> js/bitauth .generateSin (js->clj :keywordize-keys true)))
 
 (defn sign
   "Sign some data with a private-key"
   ^String [^String data, ^String priv-key]
-  (.sign js/BitAuth data priv-key))
+  (.sign js/bitauth data priv-key))
 
 (defn verify-signature
   "Verifies that a string of data has been signed."
   (^Boolean [^String data, ^String pub-key, ^String hex-signature]
-            (.verifySignature js/BitAuth data pub-key hex-signature))
+            (.verifySignature js/bitauth data pub-key hex-signature))
   (^Boolean [^String data, ^String pub-key, ^String hex-signature, call-back]
-            (.verifySignature js/BitAuth data pub-key hex-signature call-back)))
+            (.verifySignature js/bitauth data pub-key hex-signature call-back)))
 
 (defn validate-sin
   "Verify that a SIN is valid"
   (^Boolean [^String sin]
-            (.validateSin js/BitAuth sin))
+            (.validateSin js/bitauth sin))
   (^Boolean [^String sin, call-back]
-            (.validateSin js/BitAuth sin call-back)))
+            (.validateSin js/bitauth sin call-back)))
