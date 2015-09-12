ebdc57147b5881df5d120db93a5634366134b7dc
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2015-09-12 15:21:05 -0600

    Moving :dev dependencies out

--- project.clj
+++ project.clj
@@ -7,12 +7,16 @@
   :min-lein-version "2.3.4"
   :jar-exclusions [#"\.DS_Store"]
   :source-paths ["src/clj" "src/cljs"]
+  :test-paths ["test"]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.48"]
                  [com.madgag.spongycastle/core "1.52.0.0"]
                  [ring/ring-core "1.4.0"]
                  [cljsjs/bitauth "0.2.1"]
-                 [base58 "0.1.0"]]
+                 [base58 "0.1.0"]
+                 [compojure "1.4.0"]
+                 [http-kit "2.1.19"]
+                 [ring/ring-defaults "0.1.5"]]
   :plugins [[lein-cljsbuild "1.0.6"]
             [com.cemerick/clojurescript.test "0.3.3"]]
   :hooks [leiningen.cljsbuild]
@@ -28,8 +32,4 @@
   :pom-addition [:developers [:developer
                               [:id "xcthulhu"]
                               [:name "Matthew Wampler-Doty"]]]
-  :profiles {:dev {:test-paths ["test"]
-                   :dependencies [[compojure "1.4.0"]
-                                  [http-kit "2.1.19"]
-                                  [ring/ring-defaults "0.1.5"]]}}
   :target-path "target")
