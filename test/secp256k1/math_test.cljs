dddcd27d66c301b15c73704384b09a8837ce7652
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-08-13 12:02:52 -0400

    Fixing math test so it makes valid square root values

--- test/secp256k1/math_test.cljs
+++ test/secp256k1/math_test.cljs
@@ -7,7 +7,7 @@
 (deftest modular-square-root-607
   (testing "Can compute square roots modulo 607 (= 3 mod 8)"
     (let [prime 607
-          n (-> (js/Math.random) (* 1000) js/Math.floor (js/Math.pow 2))
+          n (-> (js/Math.random) (* 1000) js/Math.floor (+ 2) (js/Math.pow 2))
           root (modular-square-root n prime)]
       (is
        (= (mod n prime)
@@ -20,7 +20,7 @@
 (deftest modular-square-root-7933
   (testing "Can compute square roots modulo 7933 (= 5 mod 8)"
     (let [prime 7933
-          n (-> (js/Math.random) (* 1000) js/Math.floor (js/Math.pow 2))
+          n (-> (js/Math.random) (* 1000) js/Math.floor (+ 2) (js/Math.pow 2))
           root (modular-square-root n prime)]
       (is
        (= (mod n prime)
@@ -34,7 +34,7 @@
 (deftest modular-square-root-7937
   (testing "Can compute square roots modulo 7937 (= 1 mod 8)"
     (let [prime 7937
-          n (-> (js/Math.random) (* 1000) js/Math.floor (js/Math.pow 2))
+          n (-> (js/Math.random) (* 1000) js/Math.floor (+ 2) (js/Math.pow 2))
           root (modular-square-root n prime)]
       (is
        (= (mod n prime)
