da8237d8ef297806f6de855c3a6437599b07be6f
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-08-14 14:23:11 -0400

    Checking base conversion tests

--- test/secp256k1/tester.cljs
+++ test/secp256k1/tester.cljs
@@ -8,5 +8,4 @@
 (doo-tests 'secp256k1.core-test
            'secp256k1.math-test
            'secp256k1.formatting.der-encoding-test
-           #_'secp256k1.formatting.base-convert-test
-           )
+           'secp256k1.formatting.base-convert-test)
