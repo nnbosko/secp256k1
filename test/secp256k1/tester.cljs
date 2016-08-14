981e322f7bb2c7f27ce3068335045347a08e1219
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-08-14 18:51:17 -0400

    Testings hashes too now

--- test/secp256k1/tester.cljs
+++ test/secp256k1/tester.cljs
@@ -3,9 +3,11 @@
             [secp256k1.formatting.base-convert-test]
             [secp256k1.math-test]
             [secp256k1.core-test]
+            [secp256k1.hashes-test]
             [doo.runner :refer-macros [doo-tests]]))
 
 (doo-tests 'secp256k1.core-test
            'secp256k1.math-test
+           'secp256k1.hashes-test
            'secp256k1.formatting.der-encoding-test
            'secp256k1.formatting.base-convert-test)
